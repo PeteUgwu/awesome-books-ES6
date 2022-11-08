7a776de082bb952c1fe7eaf825d92e6fcf7ee562
peter <peterugwu64@gmail.com>
2022-11-08 11:05:03 +0100

    updated linters.yml

--- modules/interacts.js
+++ modules/interacts.js
@@ -1,31 +1,29 @@
 /* eslint-disable  max-classes-per-file */
-import Book from "./book.js";
+import Book from './book.js';
 
 export default class Display {
   static getBooks = () => {
     let books;
-    if (localStorage.getItem("books") == null) {
+    if (localStorage.getItem('books') == null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem("books"));
+      books = JSON.parse(localStorage.getItem('books'));
     }
     return books;
   };
 
   static addRemoveEvent = () => {
-    const removeBtn = document.querySelectorAll(".removeBtn");
-    removeBtn.forEach((Btn) =>
-      Btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        this.removeBook(Btn.id);
-      })
-    );
+    const removeBtn = document.querySelectorAll('.removeBtn');
+    removeBtn.forEach((Btn) => Btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.removeBook(Btn.id);
+    }));
   };
 
   static displayBooks = () => {
-    const book = document.querySelector(".books");
+    const book = document.querySelector('.books');
     const books = Display.getBooks();
-    let display = "";
+    let display = '';
     books.forEach((book, i) => {
       display += `
           <div class="booksAdded flex">
@@ -38,13 +36,13 @@ export default class Display {
   };
 
   static addBook = () => {
-    const titleInput = document.querySelector("#fname").value;
-    const authorInput = document.querySelector("#lname").value;
-    if (titleInput !== "" && authorInput !== "") {
+    const titleInput = document.querySelector('#fname').value;
+    const authorInput = document.querySelector('#lname').value;
+    if (titleInput !== '' && authorInput !== '') {
       const newBook = new Book(titleInput, authorInput);
       const books = Display.getBooks();
       books.push(newBook);
-      localStorage.setItem("books", JSON.stringify(books));
+      localStorage.setItem('books', JSON.stringify(books));
       this.displayBooks();
     }
   };
@@ -52,7 +50,7 @@ export default class Display {
   static removeBook = (id) => {
     const books = Display.getBooks();
     books.splice(id, 1);
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
     this.displayBooks();
   };
 }

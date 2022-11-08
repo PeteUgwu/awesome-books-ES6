e3b181b5726f27753bdb13c6e6945daa28eefdc9
peter <peterugwu64@gmail.com>
2022-11-08 10:11:43 +0100

    fixed peer-to-peer issues

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-import Display from './modules/interacts.js';
-import { DateTime } from './modules/luxon.js';
+import Display from "./modules/interacts.js";
+import { DateTime } from "./modules/luxon.js";
 
-const booksList = document.querySelector('.books-list');
-const addBooks = document.querySelector('.add-book');
-const contactInfo = document.querySelector('.contact-info');
-const books = document.querySelector('.books');
-const forms = document.querySelector('.form');
-const contact = document.querySelector('.contact');
-const tag = document.querySelector('h1');
-const showCurrentTime = document.querySelector('.current-time');
+const booksList = document.querySelector(".books-list");
+const addBooks = document.querySelector(".add-book");
+const contactInfo = document.querySelector(".contact-info");
+const books = document.querySelector(".books");
+const forms = document.querySelector(".form");
+const contact = document.querySelector(".contact");
+const tag = document.querySelector("h1");
+const showCurrentTime = document.querySelector(".current-time");
 
-books.style.display = 'none';
-forms.style.display = 'block';
-contact.style.display = 'none';
-tag.style.display = 'none';
+books.style.display = "none";
+forms.style.display = "block";
+contact.style.display = "none";
+tag.style.display = "none";
 
-booksList.addEventListener('click', () => {
-  forms.style.display = 'none';
-  contact.style.display = 'none';
-  books.style.display = 'block';
-  tag.style.display = 'block';
+booksList.addEventListener("click", () => {
+  forms.style.display = "none";
+  contact.style.display = "none";
+  books.style.display = "block";
+  tag.style.display = "block";
 });
-addBooks.addEventListener('click', () => {
-  forms.style.display = 'block';
-  books.style.display = 'none';
-  contact.style.display = 'none';
-  tag.style.display = 'none';
+addBooks.addEventListener("click", () => {
+  forms.style.display = "block";
+  books.style.display = "none";
+  contact.style.display = "none";
+  tag.style.display = "none";
 });
-contactInfo.addEventListener('click', () => {
-  forms.style.display = 'none';
-  books.style.display = 'none';
-  tag.style.display = 'none';
-  contact.style.display = 'block';
+contactInfo.addEventListener("click", () => {
+  forms.style.display = "none";
+  books.style.display = "none";
+  tag.style.display = "none";
+  contact.style.display = "block";
 });
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   Display.displayBooks();
 });
-const form = document.querySelector('.form');
-form.addEventListener('submit', (evt) => {
+const form = document.querySelector(".form");
+form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   Display.addBook();
   form.reset();
